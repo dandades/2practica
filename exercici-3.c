38c4a06718240f7680ff3ca869a26588076d8892
arnau <arnaurosapadros@gmail.com>
2024-10-28 09:11:47 +0100

    versio5

---
+++ exercici-3.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define N 512
+float V1[N], V2[N], V3[N], V4[N];
+float Mat[N][N], MatDD[N][N];
+
+void InitData(){
+	int i,j;
+	srand(334411);
+	for( i = 0; i < N; i++ )
+ 		for( j = 0; j < N; j++ ){
+ 			Mat[i][j]=(((i*j)%3)?-1:1)*(100.0*(rand()/(1.0*RAND_MAX)));
+ 			if ( (abs(i - j) <= 3) && (i != j))
+ 				MatDD[i][j] = (((i*j)%3) ? -1 : 1)*(rand()/(1.0*RAND_MAX));
+ 			else if ( i == j )
+ 				MatDD[i][j]=(((i*j)%3)?-1:1)*(10000.0*(rand()/(1.0*RAND_MAX)));
+ 			else MatDD[i][j] = 0.0;
+ 			}
+
+	for( i = 0; i < N; i++ ){
+ 		V1[i]=(i<N/2)?(((i*j)%3)?-1:1)*(100.0*(rand()/(1.0*RAND_MAX))):0.0;
+ 		V2[i]=(i>=N/2)?(((i*j)%3)?-1:1)*(100.0*(rand()/(1.0*RAND_MAX))):0.0;
+ 		V3[i]=(((i*j)%5)?-1:1)*(100.0*(rand()/(1.0*RAND_MAX)));
+	}
+}
+
+
+
+// -----------------------------------------------------------------------------------------------
+// EXERCICI 3
+
+void MultEscalar( float vect[N], float vectres[N], float alfa){
+
+	int i;
+	for( i = 0; i < N; i++ ){
+		vectres[i] = vect[i] * alfa;
+	}
+}
+
+int main() {
+
+	InitData();
+	float nouVector[N];
+	float num = 3.5;
+
+	MultEscalar( V1, nouVector, num);
+
+}
